ee3dccc7d488ed681208111ab0a2e7bfafb1a7cd
Skuli Dropek <66840575+skulidropek@users.noreply.github.com>
2023-03-26 23:05:39 +0300

    s
    s

---
+++ Codewars/6kata/MovingZerosToTheEnd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars._6kata
+{
+    internal class MovingZerosToTheEnd
+    {
+        public static int[] MoveZeroes(int[] arr) => arr.OrderBy(x => x == 0).ToArray();
+    }
+}
---
+++ Codewars/7kata/HighestAndLowest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars._7kata
+{
+    internal class HighestAndLowest
+    {
+        public static string HighAndLow(string numbers)
+        {
+            string[] args = numbers.Split(' ');
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                int number = int.Parse(args[i]);
+
+                if (min > number)
+                {
+                    min = number;
+                }
+
+                if (max < number)
+                {
+                    max = number;
+                }
+            }
+
+            return max + " " + min;
+        }
+    }
+}

1c4140d568de903b465bc0c9f5a6088b2b3de94b
skulidropek <66840575+skulidropek@users.noreply.github.com>
2025-01-06 22:39:56 +0400

    https://www.codewars.com/kata/55902c5eaa8069a5b4000083

---
+++ Codewars/8kata/DollarsAndCents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars._8kata
+{
+    internal class DollarsAndCents
+    {
+        public static string FormatMoney(double amount)
+        {
+            var ammountString = Math.Round(amount, 2).ToString();
+
+            if (ammountString.Contains("."))
+            {
+                if (ammountString.Split('.')[1].Length == 1)
+                {
+                    return "$" + ammountString + "0";
+                }
+
+                return "$" + ammountString;
+            }
+
+            return "$" + ammountString + ".00";
+        }
+    }
+}

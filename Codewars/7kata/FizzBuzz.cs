5e6473330248e85cdb6f2fd8816c0b944969346a
skulidropek <66840575+skulidropek@users.noreply.github.com>
2025-01-06 23:07:05 +0400

    Решил https://www.codewars.com/kata/5300901726d12b80e8000498

---
+++ Codewars/7kata/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars._7kata
+{
+    internal class FizzBuzz
+    {
+        public static string[] GetFizzBuzzArray(int n)
+        {
+            string[] result = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int index = i + 1;
+                if (index % 3 == 0 && index % 5 == 0)
+                {
+                    result[i] = "FizzBuzz";
+                }
+                else if (index % 3 == 0)
+                {
+                    result[i] = "Fizz";
+                }
+                else if (index % 5 == 0)
+                {
+                    result[i] = "Buzz";
+                }
+                else
+                {
+                    result[i] = index.ToString();
+                }
+            }
+            return result;
+        }
+    }
+}
